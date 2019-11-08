190b46f93e5fda221c522a0f8f6e027c773c3ecb
bethgranade <bgranado488@gmail.com>
2019-11-08 13:21:48 -0600

    initialize firebase in js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var index = 0;
+
+ // Initialize Firebase https://console.firebase.google.com/u/0/project/trainchoochoo-bf5aa/overview
+ var firebaseConfig = {
+  apiKey: "AIzaSyC17ZZXUarCukg4XE2z6FW-agdzSPuIDx8",
+  authDomain: "trainn-3caa9.firebaseapp.com",
+  databaseURL: "https://trainn-3caa9.firebaseio.com",
+  projectId: "trainn-3caa9",
+  storageBucket: "trainn-3caa9.appspot.com",
+  messagingSenderId: "232460108443",
+  appId: "1:232460108443:web:1fc5ac90d79e9690bffc3e",
+  measurementId: "G-MK7KL0GRRQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
